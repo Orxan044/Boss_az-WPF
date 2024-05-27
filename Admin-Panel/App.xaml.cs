0c1dadf9ea5be4a2c5fce6b0071e8ddc24cd8acf
Huseynov Orxan Samir <Husey_so01@itstep.edu.az>
2024-05-27 11:55:18 +0400

    :

---
+++ Admin-Panel/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Admin_Panel.ViewModels;
+using Admin_Panel.Views;
+using SimpleInjector;
+using System.Windows;
+
+namespace Admin_Panel;
+
+public partial class App : Application
+{
+    public static Container Container { get; set; } = new();
+    public App()
+    {
+        AddViews();
+        AddViewModels();
+    }
+
+    //private void AddOtherServices()
+    //{
+    //    Container.RegisterSingleton<INavigationService, NavigationService>();
+    //}
+
+    private void AddViewModels()
+    {
+        Container.RegisterSingleton<MainViewModel>();
+    }
+
+    private void AddViews()
+    {
+        Container.RegisterSingleton<MainView>();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var mainView = Container.GetInstance<MainView>();
+        mainView.DataContext = Container.GetInstance<MainViewModel>();
+        mainView.Show();
+        base.OnStartup(e);
+    }
+
+}
